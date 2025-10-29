9de1ea7253768a50889a1a8a3c471337d55a22b7
GabrielG2006 <gabrielguedes20066@gmail.com>
2025-10-29 06:39:13 -0300

    Desafio Trilhas .NET - API

--- Program.cs
+++ Program.cs
@@ -1,33 +1,55 @@
+using System;
+using System.Linq;
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Context;
+using TrilhaApiDesafio.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+
+var conexao = builder.Configuration.GetConnectionString("ConexaoPadrao");
+if (!string.IsNullOrWhiteSpace(conexao) && !conexao.Contains("COLOCAR"))
+{
+    builder.Services.AddDbContext<OrganizadorContext>(options =>
+        options.UseSqlServer(conexao));
+}
+else
+{
+  
+    builder.Services.AddDbContext<OrganizadorContext>(options =>
+        options.UseInMemoryDatabase("TrilhaDb"));
+}
 
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<OrganizadorContext>();
+    if (!context.Tarefas.Any())
+    {
+        context.Tarefas.AddRange(
+            new Tarefa { Titulo = "Exemplo 1", Descricao = "Tarefa de exemplo 1", Data = DateTime.Now, Status = EnumStatusTarefa.Pendente },
+            new Tarefa { Titulo = "Exemplo 2", Descricao = "Tarefa de exemplo 2", Data = DateTime.Now.AddDays(1), Status = EnumStatusTarefa.Finalizado }
+        );
+        context.SaveChanges();
+    }
+}
 
 app.Run();
